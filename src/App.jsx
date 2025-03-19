b408c53f927a4b96ba4ce5b39fe1dc19e1979350
Razieh Abedi <raziyeabedi@gmail.com>
2025-03-19 14:00:52 +0800

    added the game board section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
+import { useState } from "react";
+
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
- 
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  const handleSelectSquare = () => {
+    setActivePlayer((currActivePlayer) =>
+      currActivePlayer === "X" ? "O" : "X"
+    );
+  };
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
+        <ol id="players" className="highlight-player">
           <Player
             initialName="player1"
             symbol="X"
+            isActive={activePlayer === "X"}
           />
           <Player
             initialName="player2"
             symbol="O"
+            isActive={activePlayer === "O"}
           />
         </ol>
-        game board
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
       logs
     </main>
